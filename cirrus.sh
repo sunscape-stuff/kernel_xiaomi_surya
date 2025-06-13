e9a5176657230ae02ead13b2edd72a99606410b7
David112x <86davidx64@gmail.com>
2025-06-13 13:12:51 -0500

    CI: install missing deps
    * libxml2 was missing, get it
    * aarch64 binutils may be necessary as seen from the logs
    "which: no aarch64-linux-gnu-elfedit"
    
    Signed-off-by: David112x <86davidx64@gmail.com>

--- cirrus.sh
+++ cirrus.sh
@@ -3,7 +3,7 @@
 # Before building the kernel, first we need to update and then install a few packages on Cirrus CI and so we'll do that.
 pacman -Sy --noconfirm
 pacman -S archlinux-keyring --noconfirm
-pacman -Syu --needed --noconfirm wget base-devel xmlto inetutils bc cpio python-sphinx python-sphinx_rtd_theme graphviz imagemagick git python zip github-cli fortune-mod ccache jre8-openjdk
+pacman -Syu --needed --noconfirm aarch64-linux-gnu-binutils wget base-devel xmlto libxml2 inetutils bc cpio python-sphinx python-sphinx_rtd_theme graphviz imagemagick git python zip github-cli fortune-mod ccache jre8-openjdk
 # After installing the packages, we can move onto calling the build script.
 
 bash cirrus_build.sh
